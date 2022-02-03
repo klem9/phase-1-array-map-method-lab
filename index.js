cd32283d2793635f66fd2362900559875890027d
Klemense <klemenseloh@gmail.com>
2022-02-03 17:26:08 -0500

    commit tree 6dbb9b762b336279159d38f96d5133b332ce16ee

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+/* const titleCased = () => {
+  return tutorials.map((titleCase) => {
+    return titleCase.split(" ").map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+     }).join(" ")
+  })
+} */
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((titleCase) => {
+    return str = titleCase.split(' ')
+  })
 }
+console.log(titleCased())
